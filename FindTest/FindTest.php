1e99e80c9b0139910ba1e279f04635ad685c178a
vtklisurov <52920886+vtklisurov@users.noreply.github.com>
2021-06-10 19:47:13 +0300

    Update FindTest.php

--- FindTest/FindTest.php
+++ FindTest/FindTest.php
@@ -21,9 +21,9 @@
             {
                 $searchValue    = filter_var($_POST["searchVal"], FILTER_SANITIZE_STRING);
 
-                $serverName     = "78.90.25.79:3308";
+                $serverName     = "http://ec2-52-54-235-211.compute-1.amazonaws.com/:3306";
                 $userName       = "root";
-                $password       = "";
+                $password       = "todor is my dad";
                 $dbName         = "audio_tests";
 
                 // Create connection
