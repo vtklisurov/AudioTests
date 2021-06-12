e9d2c58a460f4141d21778f4ad790955e9d6dcd2
vtklisurov <vtklisurov@gmail.com>
2021-06-12 22:43:56 +0300

    Update saveTest.php

--- CreateTest/saveTest.php
+++ CreateTest/saveTest.php
@@ -1,5 +1,5 @@
 <?php
-header('Content-Type: text/html; charset=utf-8');
+header('Content-Type: text/html\n\n; charset=utf-8');
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
