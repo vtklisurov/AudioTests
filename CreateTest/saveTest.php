20a8c9a0eaf94361ac99b1444fa185bbbb6684bd
vtklisurov <vtklisurov@gmail.com>
2021-06-12 22:18:33 +0300

    Update saveTest.php

--- CreateTest/saveTest.php
+++ CreateTest/saveTest.php
@@ -1,6 +1,5 @@
 <?php
-header('charset=utf-8');
-header('Content-Type: text/html\n\n');
+header('Content-Type: text/html\n\n; charset=utf-8');
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -262,6 +261,7 @@ $stmt->close();
 $conn->close();
 
 //output a message on the browser and redirect back to the homepage
+header('Content-Type: text/html\n\n; charset=utf-8');
 echo "<!DOCTYPE html>
 <html>
 <body style='text-align:center; background-color: #EDE7E3;'>
