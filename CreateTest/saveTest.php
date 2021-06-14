a791eef9ad32ce4bc20ac721852f049bd32ad7a7
vtklisurov <vtklisurov@gmail.com>
2021-06-14 18:00:05 +0300

    Update saveTest.php

--- CreateTest/saveTest.php
+++ CreateTest/saveTest.php
@@ -95,7 +95,7 @@ for ($x = 0; $x <= count($_POST["type"])-1; $x++) {
 		}
 		
 		//save the file if it exists
-		if (isset($_FILES['question_audio']['tmp_name'][$x])) {
+		if (isset($_FILES['question_audio']['tmp_name'][$x]) && $_FILES['question_audio']['tmp_name'][$x] != "") {
 			//move_uploaded_file($_FILES["question_audio"]["tmp_name"][$x], $audioPath . $id . "-". $audioindex++ . '.' . @end((explode(".", $_FILES["question_audio"]["name"][$x]))) );
 			
 			$s3->putObject([
