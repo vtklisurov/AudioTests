c29595473369a6c9bba0d2025c4037117f561b1c
vtklisurov <vtklisurov@gmail.com>
2021-06-14 15:56:46 +0300

    Update saveTest.php

--- CreateTest/saveTest.php
+++ CreateTest/saveTest.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
-require '../../../../../home/ec2-user/vendor/autoload.php';
+require '../vendor/autoload.php';
 
 //ini_set('display_errors', 1);
 //ini_set('display_startup_errors', 1);
