b45cf5d74b4e7c466b6d2a15e35f7fd5bef38420
vtklisurov <vtklisurov@gmail.com>
2021-06-14 17:44:23 +0300

    Update credentials.php

--- credentials.php
+++ credentials.php
@@ -3,7 +3,7 @@
     $userName       = "admin";
     $password       = "shestichko1";
     $dbName         = "audio_tests";
-	$key            ="ASIA5OWU37NQPB4TBU5C";
-	$secretkey      ="LhedRHW0qNW1531UV53GXdfCvpxpq/N8fNLxjqqZ";
-	$sessiontoken   ="FwoGZXIvYXdzEF0aDPcmn5AtoAK0ddEv6yLHAQ7V3ftLSWy6VknopA3BvRkNvgKpR3873DfYVC1OHuN53lYrA/z6K36+RZso9bGOG+V8ZG8/sZYHFRzidcKM6+91w9vHP66W2uB3VmeSYgtXiE9PzvJWakpFf7Og1qcoltBXYDj7V2q6k0wjJK2Q9ldl+zAXd6J3gyoNzwFctas6UfW20dhUMN6bdAFA+zj/sC0toB8a+eaGBpilcrvfhP7nQL7bvS+/Nhq/AyAlVc0XUXAk/7ynHABuxemVJDdoG30eic//klootoGdhgYyLauxsL3qTZQdKKwWlicLPLymxaoS7eSlA/oiU/iFi+f1GNsLnL2o7f9v5NztzQ==";
+	$key            ="ASIA5OWU37NQOLFHTBHN";
+	$secretkey      ="G8288jEYQTJ8EoIO/C2UL4vANpejkFtc7wKKmWxQ";
+	$sessiontoken   ="FwoGZXIvYXdzEGAaDFXZ6I0mk1nlq2yHJyLHAZ4XU4XgJ6CO90NUI0paPmYUwVMs8/JoF60CdJx4nWPxmkwUTKIMbkOyP2X8Pw1CqopvVNrfmnXbhWm+HsfNWW8Qc4bYlLfFFcT5oIpddrxs8vv+8qz5TEWXMs6EJLLw+y+hQQ42kR72kSoEr2aLty0H0urLCIJA1FFrnoMruCMp06+ks+tu6T8wmPzgmnb9F6tjf0lxFjZxOo43uegVDqTadslWIeLM8Lrwh5vCnCERysxjW00iAmy374hQdC7ZDB7yawNCy0gonNadhgYyLfo2WTtkAsYV/839hOQa2oma1xyMbNk8cKDQ24AeKDIC5eOno+M3mHZDMDhDNg==";
 ?>
