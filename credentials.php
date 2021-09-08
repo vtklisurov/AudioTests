a928741c5a2d131242d3657af126325c588879ae
vtklisurov <52920886+vtklisurov@users.noreply.github.com>
2021-09-08 19:26:43 +0300

    Delete credentials.php

--- credentials.php
+++
@@ -1,9 +0,0 @@
-<?php	
-	$serverName     = name:port;
-    	$userName       = uname;
-    	$password       = pass;
-    	$dbName         = dbname;
-	$key            = key;
-	$secretkey      = secretkey;
-	$sessiontoken   = token;
-?>
