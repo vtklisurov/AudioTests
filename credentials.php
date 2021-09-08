fab93880cd0563772bdaf593adf1232e8cd3547a
vtklisurov <52920886+vtklisurov@users.noreply.github.com>
2021-09-08 19:23:28 +0300

    Update credentials.php

--- credentials.php
+++ credentials.php
@@ -1,9 +1,9 @@
 <?php	
-	$serverName     = "database-audio-tests.chvvgimintvl.us-east-1.rds.amazonaws.com:3306";
-    $userName       = "admin";
-    $password       = "shestichko1";
-    $dbName         = "audio_tests";
-	$key            ="ASIA5OWU37NQOU4LEHZR";
-	$secretkey      ="WghK2JViMpeR1vBZ0inibwZoE1zyy6ZbT0G7Z5Pa";
-	$sessiontoken   ="FwoGZXIvYXdzEHYaDIv/jga9z8j+RWpAPCLHAd4XIl2v/YBmB73Ze6pQMliuy/rRK5rtYKg9l32CaWZA5nPgNaYsMtYdd8mQB10doIcuWsLbNKGd9nqiMfOH381HfHrYLz7wXJxg37/aeTr8nPrDGxjVpVaCj+vKYH54+f7qrUFsNhzUsTJngM27t3zbf+06M6K+AWEIDfK/gIAJcFMI8Plx110wOvYt2TRw8K8Kx5uVsK2cha+oqx/uFj3wwCvE51T856SaONUsKlUlKMUAFNLa+5Ls0LGm6VpE+0G+sATsNoIo5cmihgYyLRHJizZdqaRvDJhDF7d8cMr7d6H+ZiHTCRbwhbPgNpaEQaRh444bQNjZma5hiQ==";
+	$serverName     = name:port;
+    	$userName       = uname;
+    	$password       = pass;
+    	$dbName         = dbname;
+	$key            = key;
+	$secretkey      = secretkey;
+	$sessiontoken   = token;
 ?>
